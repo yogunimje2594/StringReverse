364597cbc3a9474215661bf61473163039987878
yogeshnimje <39332931+yogeshnimje@users.noreply.github.com>
2019-02-09 21:46:59 +0530

    First commit in Project

---
+++ src/com/app/Test.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Test{
+	public static void main(String[] args) {
+		List<Integer> list=Arrays.asList(1,2,3,4,5);
+		Collections.reverse(list);
+		System.out.println(list);
+		
+	}
+}
